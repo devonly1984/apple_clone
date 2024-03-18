e812ecd3b8b0971fc88deec2a65fd3dbc34f82bc
unknown <PCGUARD@PON2027486D.safeway01.ad.safeway.com>
2024-03-18 17:41:34 -0500

    Sentry starter

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://3149ae7f8e287a39afd9a75319cbfd12@o4506934398353408.ingest.us.sentry.io/4506934401368064",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+ 
+  tracesSampleRate: 1.0,  
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
